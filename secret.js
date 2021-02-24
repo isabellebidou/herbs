1d81b28a17a8580e548cd178ef5da913739829f1
Isabelle <isa.bidou@gmail.com>
2021-02-24 22:41:00 +0100

    2nd commit to heroku with passport login

--- secret.js
+++ secret.js
@@ -4,21 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 var mysql = require("mysql");
-// const db = mysql.createConnection({
-//   host: "isabellebidou.com",
-//   user: "isabelle_2",
-//   password: "TheDuck21",
-//   database: "isabelle_db",
-//   port: 3306,
-// });
-
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DB,
-  port: process.env.DB_PORT,
+  host: "isabellebidou.com",
+  user: "isabelle_2",
+  password: "TheDuck21",
+  database: "isabelle_db",
+  port: 3306,
 });
-module.exports = {
-  db: db,
-};
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DB,
+//   port: process.env.DB_PORT,
+// });
+// module.exports = {
+//   db: db,
+// };
