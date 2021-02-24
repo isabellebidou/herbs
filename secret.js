30554e80863bf6e38e5c703ba23bd49f6e3dc66e
Isabelle bidou <isa.bidou@gmail.com>
2021-02-24 23:21:33 +0100

    Update secret.js

--- secret.js
+++ secret.js
@@ -4,13 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 var mysql = require("mysql");
-// const db = mysql.createConnection({
-//   host: "isabellebidou.com",
-//   user: "isabelle_2",
-//   password: "TheDuck21",
-//   database: "isabelle_db",
-//   port: 3306,
-// });
+
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
